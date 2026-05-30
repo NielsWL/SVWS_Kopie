df95911ce8c83a0722597e9dd6d7eca96101ad6e
NielsWL <71388959+NielsWL@users.noreply.github.com>
2026-05-30 21:59:18 +0200

    Fix Q2.1 course blocking rules

--- svws-webclient/client/src/components/gost/kursplanung/SGostKursplanungBlockungAuswahl.vue
+++ svws-webclient/client/src/components/gost/kursplanung/SGostKursplanungBlockungAuswahl.vue
@@ -28,7 +28,7 @@
 					<div class="-my-1 ml-auto inline-flex gap-1">
 						<template v-if="hatUpdateKompetenz && ((auswahlBlockung !== undefined) && !isPending(auswahlBlockung.id)) && (blockung === auswahlBlockung)">
 							<template v-if="allow_berechne_blockung">
-								<s-gost-kursplanung-modal-blockung-ausfuehrlich-berechnen v-if="allow_berechne_blockung" :map-core-type-data :ausfuehrliche-darstellung-kursdifferenz :set-ausfuehrliche-darstellung-kursdifferenz :get-datenmanager :add-ergebnisse v-slot="{ openModal }">
+								<s-gost-kursplanung-modal-blockung-ausfuehrlich-berechnen v-if="allow_berechne_blockung" :map-core-type-data :ausfuehrliche-darstellung-kursdifferenz :set-ausfuehrliche-darstellung-kursdifferenz :get-datenmanager :get-ergebnismanager :add-ergebnisse v-slot="{ openModal }">
 									<svws-ui-button type="transparent" @click="openModal" title="Ausführliche Berechnung lokal im Browser und Auswahl von guten Ergebnissen" :disabled="apiStatus.pending" class="text-ui-100 mr-4">
 										<span class="icon-sm i-ri-calculator-line -mx-0.5" /> Blocken…
 									</svws-ui-button>
